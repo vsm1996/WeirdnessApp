c663dcfdb79d56ae85fa7bdc319bace0cb13a43f
Vanessa Martin <vanessa@shopvida.com>
2019-05-09 09:12:11 -0700

    seperated API key from main code, put Header in its own component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = props => {
+  return (
+    <header className="App-header">
+      <h1>Weirdness Calculator</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/utils/Key.js
@@ -0,0 +1,3 @@
+const Key = "iNfVNqEv4pF2luxPCXT51odSlQA7nIC2";
+
+export default Key;
